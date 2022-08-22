ed25a052707d9509020e66df312057f905019388
dima <testImplementation>
2022-08-23 01:28:49 +0500

    add application.conf config

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,14 @@
 package com.example
 
-import io.ktor.server.engine.*
+
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.application.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
 }
